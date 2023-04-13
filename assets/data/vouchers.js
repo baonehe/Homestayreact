f16abc33324eb20d0e22e4237669042d5fff833b
VietQuoc29 <20520725@gm.uit.edu.vn>
2023-04-13 13:32:44 +0700

    Rewards UI & firebase

---
+++ assets/data/vouchers.js
@@ -0,0 +1,64 @@
+const vouchers = [
+    {
+        id: '1',
+        name: 'New Member',
+        title: 'Quà đăng ký mới',
+        sale_off:'55%',
+        date_start: '30/03/2023',
+        date_end: '29/04/2023',
+        quantity: 1,
+        notice: 'Áp dụng tất cả các hình thức thanh toán',
+        details:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    },
+    {
+        id: '2',
+        name: 'Couples',
+        title: 'Những cuộc yêu nồng cháy',
+        sale_off:'5%',
+        date_start: '30/03/2023',
+        date_end: '01/06/2023',
+        quantity: 1,
+        notice: 'Áp dụng thanh toán online',
+        details:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    },
+    {
+        id: '3',
+        name: 'Only FA',
+        title: 'Vô weitei',
+        sale_off:'100%',
+        date_start: '30/03/2023',
+        date_end: '20/04/2023',
+        quantity: 2,
+        notice: 'Áp dụng thanh toán trực tiếp',
+        details:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    },
+    {
+        id: '4',
+        name: 'FWB',
+        title: 'Friend with Bún bò',
+        sale_off:'20%',
+        date_start: '30/03/2023',
+        date_end: '21/04/2023',
+        quantity: 1,
+        notice: 'Áp dụng thanh toán online',
+        details:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    },
+    {
+        id: '5',
+        name: 'ONS',
+        title: 'Ông Năm Sao',
+        sale_off:'40.000đ',
+        date_start: '30/03/2023',
+        date_end: '22/04/2023',
+        quantity: 1,
+        notice: 'Áp dụng tất cả các hình thức thanh toán',
+        details:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    },
+];
+
+export default vouchers;
--- components/RewardsScreen.js
+++ components/RewardsScreen.js
@@ -1,11 +1,168 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
+import colors from '../assets/consts/colors';
+import vouchers from '../assets/data/vouchers';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 const Rewards = () => {
+
+    const VoucherCard = ({ voucher }) => {
+        return (
+            <TouchableOpacity>
+                <View style={styles.voucherCard}>
+                    <View style={styles.voucher_title}>
+                        <View style={styles.voucher_name}>
+                        <Text style={styles.txt_name}
+                        >{voucher.title}</Text>
+                        </View>
+                        <Text style={styles.voucher_quantity}>{voucher.quantity}x</Text>
+                    </View>
+                    <View style={styles.voucher_content}>
+                        <View style={styles.voucher_saleoff}>
+                            <Text style={styles.txt_saleoff}>{voucher.sale_off}</Text>
+                            <Text style={styles.txt_date}>{voucher.date_start} ~ {voucher.date_end}</Text>
+                        </View>
+                        <TouchableOpacity style={styles.useTouch}>
+                            <Text style={styles.txt_use}>DÙNG</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.voucher_notice}>
+                        <Icon name="md-information-circle-sharp" size={20} color={colors.primary} />
+                        <Text style={styles.txt_notice}>{voucher.notice}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
     return (
-        <View>
-            <Text>Rewards</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Text style={styles.title}>REWARDS</Text>
+            </View>
+            <FlatList
+                data={vouchers}
+                vertical
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={styles.flatList}
+                renderItem={({ item }) => <VoucherCard voucher={item} />}
+            />
+        </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: colors.white,
+    },
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontFamily: 'Inter-Bold',
+        fontSize: 30,
+        color: colors.dark,
+    },
+    flatList: {
+        marginTop: 8,
+        paddingLeft: 20,
+        paddingHorizontal: 10,
+        paddingBottom: 120,
+    },
+    voucherCard: {
+        height: 200,
+        width: '88%',
+        backgroundColor: colors.white,
+        elevation: 15,
+        marginHorizontal: 10,
+        borderRadius: 10,
+        marginBottom: 20,
+        borderWidth: 1,
+        borderColor: colors.gray,
+    },
+    voucher_title: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    voucher_name: {
+        width: '85%',
+        borderTopLeftRadius: 8,
+        backgroundColor: colors.dark,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    txt_name: {
+        fontSize: 15,
+        fontFamily: 'Inter-Medium',
+        textAlign: 'center',
+        color: colors.lightwhite,
+    },
+    voucher_quantity: {
+        width: '15%',
+        borderTopRightRadius: 8,
+        fontSize: 18,
+        fontFamily: 'Inter-Medium',
+        color: colors.lightwhite,
+        backgroundColor: colors.primary,
+        textAlign: 'center',
+        justifyContent: 'center',
+    },
+    voucher_content: {
+        flex: 5,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    voucher_saleoff: {
+        width: '85%',
+        borderRightWidth: 2,
+        borderStyle: 'dashed',
+        borderColor: colors.gray,
+        fontFamily: 'Lato-Bold',
+    },
+    txt_saleoff: {
+        fontSize: 26,
+        fontFamily: 'Inter-SemiBold',
+        color: colors.black,
+        marginTop: 10,
+        marginLeft: 20,
+    },
+    txt_date: {
+        fontSize: 16,
+        fontFamily: 'Inter-Regular',
+        marginTop: 30,
+        marginLeft: 20,
+    },
+    useTouch: {
+        width: '15%',
+        fontSize: 20,
+        fontFamily: 'Inter-Regular',
+        justifyContent: 'center',
+    },
+    txt_use:{
+        color: colors.primary,
+        fontSize: 14,
+        fontFamily: 'Inter-ExtraBold',
+        textAlign: 'center',
+    },
+    voucher_notice: {
+        height: 'auto',
+        flexDirection: 'row',
+        backgroundColor: '#cfe8ef',
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    txt_notice: {
+        fontFamily: 'Inter-Semibold',
+        color: colors.primary,
+        marginLeft: 3,
+    },
+});
+
 export default Rewards;
